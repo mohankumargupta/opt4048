da272f99b8c6d8e6075210dd2c9039df9ff5bf16
Mohan Gupta <mohangupta@live.com>
2024-04-18 16:19:28 +1000

    can read lux

--- examples/lux.rs
+++ examples/lux.rs
@@ -8,4 +8,6 @@ fn main() {
   let device_id = opt4048.read_device_id().unwrap();
   let id: u16 = device_id[0] as u16 * 256 + device_id[1] as u16;
   println!("0: {} 1: {} 2: {}", device_id[0], device_id[1], id);
+  let lux = opt4048.read_lux().unwrap();
+  println!("lux: {}", lux);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+
 use embedded_hal::i2c;
 
 const I2C_ADDRESS: u8 = 0x44;
@@ -30,18 +31,16 @@ const M1Z: f32 = -0.0000169739553;
 const M2X: f32 = 0.0000120811684;
 const M2Y: f32 = -0.0000158848115;
 const M2Z: f32 = 0.000674021520;
-const LUX_MATRIX: f32 = 0.00215;
-
-
+const M1L: f32 = 0.00215; //lux
 
 //MANTISSA_CHx=(RESULT_MSB_CHx<<8) + RESULT_LSB_CHx
 //RESULT_MSB_CHx register carries the most significant 12 bits of the MANTISSA_CHx
 //RESULT_LSB_CHx register carries the least significant 8 bits of the MANTISSA_CHx.
-//MANTISSA_CHx is then computed using the above equations to get the 20 bit number. 
+//MANTISSA_CHx is then computed using the above equations to get the 20 bit number.
 //EXPONENT_CHx is directly read from the register which is 4 bits.
 //ADC_CODES_CHx = (MANTISSA_CHx<<EXPONENT_CHx)
 pub struct OPT4048<I2C> {
-    i2c: I2C
+    i2c: I2C  
 }
 
 #[derive(Debug)]
@@ -52,33 +51,34 @@ pub enum OPT4048Error<E> {
     InvalidInputData,
 }
 
-impl<I2C> OPT4048<I2C> 
-where I2C: i2c::I2c,
+impl<I2C> OPT4048<I2C>
+where
+    I2C: i2c::I2c,
 {
-  pub fn new(i2c: I2C) -> Self {
-    Self{i2c}
+    pub fn new(i2c: I2C) -> Self {
+        Self { i2c }
+    }
+
+    pub fn read_device_id(&mut self) -> Result<[u8; 2], OPT4048Error<I2C::Error>> {
+        let mut id = [0, 0];
+        self.i2c
+            .write_read(I2C_ADDRESS, &[OPT4048_REGISTER_DEVICE_ID], &mut id)
+            .map_err(OPT4048Error::I2C)?;
+        Ok(id)
+    }
+
+    pub fn read_lux(&mut self) -> Result<f32, OPT4048Error<I2C::Error>> {
+        let mut block = [0u8; 4];
+        self.i2c
+            .write_read(I2C_ADDRESS, &[OPT4048_REGISTER_CHANNEL1], &mut block)
+            .map_err(OPT4048Error::I2C)?;
+          
+        let exponent_ch1 = (block[0] & 0xF0) >> 4;
+        let result_msb_ch1 = ((block[0] & 0x0F) as u16) << 8 | (block[1] as u16);
+        let result_lsb_ch1 = block[2] as u16;
+        let mantissa_ch1: u32 = result_msb_ch1 as u32 * 256 + result_lsb_ch1 as u32;
+        let adc_ch1 = mantissa_ch1 << exponent_ch1;
+        let lux = adc_ch1 as f32 * M1L;
+        Ok(lux)
+    }
   }
-
-  pub fn read_device_id(&mut self) -> Result<[u8;2], OPT4048Error<I2C::Error>> {
-    let mut id = [0, 0];
-    self.i2c.write_read(I2C_ADDRESS,&[OPT4048_REGISTER_DEVICE_ID], &mut id).map_err(OPT4048Error::I2C)?; 
-    Ok(id)
-  }
-
-  pub fn read_lux(&mut self) -> Result<f32, OPT4048Error<I2C::Error>>  {
-    let mut register_offset_2h = [0,0];
-    let mut register_offset_3h = [0];
-    self.i2c.write_read(I2C_ADDRESS,&[OPT4048_REGISTER_CHANNEL1], &mut register_offset_2h).map_err(OPT4048Error::I2C)?;   
-    self.i2c.write_read(I2C_ADDRESS,&[OPT4048_REGISTER_CHANNEL1_EXTRA], &mut register_offset_3h).map_err(OPT4048Error::I2C)?;   
-    
-    let raw_2h = u16::from_be_bytes(register_offset_2h);
-    let result_msb_ch1 = raw_2h & 0x0FFF;
-    let exponent_ch1 = raw_2h & 0xF000 >> 12;
-    let result_lsb_ch1 = register_offset_3h[0];
-    let mantissa_ch1=(result_msb_ch1<<8) + result_lsb_ch1 as u16;
-    let adc_codes_ch1 = mantissa_ch1<<exponent_ch1;
-    Ok(LUX_MATRIX * adc_codes_ch1 as f32)
-  }
-
-}
-
